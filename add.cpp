5c55cce2331c7e78d289aa6bfc33143d189e7609
nichitharshad <95277838+nichitharshad@users.noreply.github.com>
2022-05-10 23:27:11 +0530

    1st cpp program

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the first no.:";
+    cin>>a;
+    cout<<"Enter the second no.:";
+    cin>>b; 
+    c=a+b;
+    cout<<"The sum of the given no.s is:"<<c;
+    
+}
